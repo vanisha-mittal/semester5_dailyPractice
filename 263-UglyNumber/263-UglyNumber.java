0c70cf3655d916afb838df6191cf2e99c64f8654
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:22:11 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,21 @@
+// Last updated: 7/31/2025, 5:21:33 PM
+class Solution {
+    public boolean isUgly(int n) {
+        if(n<1){
+            return false;
+        }
+        while(n%2==0){
+            n=n/2;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        while(n%5==0){
+            n=n/5;
+        }
+        if(n==1){
+            return true;
+        }
+        return false;
+    }
+}
