a22c780da2575cfaa07ce001b75d473469898974
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:20:12 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,34 @@
+// Last updated: 7/31/2025, 5:19:34 PM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        for(int i:bills){
+            if(i==5){
+                five++;
+            }
+            else if(i==10){
+                if(five>0){
+                    ten++;
+                    five--;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(five>0 && ten>0){
+                    five--;
+                    ten--;
+                }
+                else if(five>2){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
