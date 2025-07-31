f5234509b084595fca3d6b55ccc29652cf4ad772
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:22:43 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,14 @@
+// Last updated: 7/31/2025, 5:22:06 PM
+class Solution {
+    public int rob(int[] nums) {
+        int rob = 0;
+        int norob = 0;
+        for (int i = 0; i < nums.length; i ++) {
+            int newRob = norob + nums[i];
+            int newNoRob = Math.max(norob, rob);
+            rob = newRob;
+            norob = newNoRob;
+        }
+        return Math.max(rob, norob);
+    }
+}
