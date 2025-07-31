f77bc59bc5a6501813e4de3befbab2734bb56e32
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:19:32 +0530

    Create 1411-ConvertBinaryNumberInALinkedListToInteger

---
+++ 1411-ConvertBinaryNumberInALinkedListToInteger/1411-ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,11 @@
+// Last updated: 7/31/2025, 5:18:55 PM
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int num = 0;
+        while (head != null) {
+            num = num * 2 + head.val;
+            head = head.next;
+        }
+        return num;
+    }
+}
