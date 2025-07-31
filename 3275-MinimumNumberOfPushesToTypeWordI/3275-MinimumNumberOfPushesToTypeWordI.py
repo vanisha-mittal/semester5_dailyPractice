18bed46a970808b1ed114b141835434b8f7709c1
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:13:15 +0530

    Create 3275-MinimumNumberOfPushesToTypeWordI

---
+++ 3275-MinimumNumberOfPushesToTypeWordI/3275-MinimumNumberOfPushesToTypeWordI.py
@@ -0,0 +1,15 @@
+# Last updated: 7/31/2025, 5:12:37 PM
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        arr=[0]*26
+        for i in range(26):
+            arr[i]=word.count(chr(97+i))
+        arr.sort(reverse=True)
+        ans=0
+        for i in range(26):
+            if arr[i]==0:
+                break
+            ans+=(i//8+1)*arr[i]
+        return ans
+
+        
