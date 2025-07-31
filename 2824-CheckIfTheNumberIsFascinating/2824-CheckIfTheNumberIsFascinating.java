c6423db5c28773c0c47ae7b119c0c3c14d9181ac
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:14:03 +0530

    Create 2824-CheckIfTheNumberIsFascinating

---
+++ 2824-CheckIfTheNumberIsFascinating/2824-CheckIfTheNumberIsFascinating.java
@@ -0,0 +1,9 @@
+// Last updated: 7/31/2025, 5:13:26 PM
+class Solution {
+    public boolean isFascinating(int n) {
+        if(n==192||n==273||n==327||n==219){
+            return true;
+        }
+        return false;
+    }
+}
