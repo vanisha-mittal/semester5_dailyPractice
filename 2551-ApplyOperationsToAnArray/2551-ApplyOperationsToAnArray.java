e8852b849efb30cdb59df191962d01df9b18340d
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:15:45 +0530

    Create 2551-ApplyOperationsToAnArray

---
+++ 2551-ApplyOperationsToAnArray/2551-ApplyOperationsToAnArray.java
@@ -0,0 +1,23 @@
+// Last updated: 7/31/2025, 5:15:07 PM
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int[] result = new int[nums.length];
+        int count = 0;
+        int i;
+        for(i = 0; i < nums.length - 1; i++){
+            if(nums[i] != 0){
+                if(nums[i] == nums[i + 1]){
+                    result[count++] = nums[i] * 2;
+                    i++;
+                }
+                else{
+                    result[count++] = nums[i];
+                }
+            }
+        }
+        if(i != nums.length){
+            result[count] = nums[nums.length - 1];
+        }
+        return result;
+    }
+}
