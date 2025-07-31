b769230cdb0121b54430ad494e6e87f752f4ad37
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:14:47 +0530

    Create 2734-ArrayPrototypeLast

---
+++ 2734-ArrayPrototypeLast/2734-ArrayPrototypeLast.js
@@ -0,0 +1,17 @@
+// Last updated: 7/31/2025, 5:14:09 PM
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length===0){
+        return -1;
+    }
+    else{
+        return this[this.length-1];
+    }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
