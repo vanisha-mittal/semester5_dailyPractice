1b154c9c8b326450aff8110bdb395badff2a59b9
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-07-31 17:09:56 +0530

    Create 3832-MinimumCostToReachEveryPosition

---
+++ 3832-MinimumCostToReachEveryPosition/3832-MinimumCostToReachEveryPosition.java
@@ -0,0 +1,16 @@
+// Last updated: 7/31/2025, 5:09:18 PM
+class Solution {
+    public int[] minCosts(int[] cost) {
+        int[] ans=new int[cost.length];
+        ans[0]=cost[0];
+        for(int i=1;i<cost.length;i++){
+            if(cost[i]>ans[i-1]){
+                ans[i]=ans[i-1];
+            }
+            else{
+                ans[i]=cost[i];
+            }
+        }
+        return ans;
+    }
+}
