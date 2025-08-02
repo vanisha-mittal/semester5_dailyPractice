f39aba3dbda41f572c69b357325e995a2f36976f
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-02 16:04:27 +0530

    Create 95-UniqueBinarySearchTreesIi

---
+++ 95-UniqueBinarySearchTreesIi/95-UniqueBinarySearchTreesIi.java
@@ -0,0 +1,44 @@
+// Last updated: 8/2/2025, 4:03:49 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        return generateSubtrees(1, n);
+    }
+
+    private List<TreeNode> generateSubtrees(int s, int e) {
+        List<TreeNode> res = new LinkedList<TreeNode>();
+        if (s > e) {
+            res.add(null);
+            return res;
+        }
+
+        for (int i = s; i <= e; ++i) {
+            List<TreeNode> leftSubtrees = generateSubtrees(s, i - 1);
+            List<TreeNode> rightSubtrees = generateSubtrees(i + 1, e);
+
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
