8ec51acb3ef3f8414c8dd4d844e84e7ca9e85d56
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-02 16:05:23 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.py
@@ -0,0 +1,11 @@
+# Last updated: 8/2/2025, 4:04:45 PM
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        return x**n
+    
+        
