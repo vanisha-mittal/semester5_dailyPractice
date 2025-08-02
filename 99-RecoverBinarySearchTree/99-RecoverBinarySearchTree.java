15510b8307cff1e33c55f8415ede14220ea2a94c
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-02 16:04:23 +0530

    Create 99-RecoverBinarySearchTree

---
+++ 99-RecoverBinarySearchTree/99-RecoverBinarySearchTree.java
@@ -0,0 +1,39 @@
+// Last updated: 8/2/2025, 4:03:45 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev = null, first = null, second = null;
+
+    public void recoverTree(TreeNode root) {
+        evalSwappedNodes(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void evalSwappedNodes(TreeNode curr) {
+        if (curr == null)
+            return;
+        evalSwappedNodes(curr.left);
+        if (prev != null && prev.val > curr.val) {
+            if (first == null)
+                first = prev;
+            second = curr;
+        }
+        prev = curr;
+        evalSwappedNodes(curr.right);
+    }
+}
