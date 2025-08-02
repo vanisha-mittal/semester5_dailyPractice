2832a358c35984503363e7e9291a37bb106d2f66
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-02 16:05:31 +0530

    Create 43-MultiplyStrings

---
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -0,0 +1,11 @@
+# Last updated: 8/2/2025, 4:04:53 PM
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        return str(int(num1)*int(num2))
+
+        
