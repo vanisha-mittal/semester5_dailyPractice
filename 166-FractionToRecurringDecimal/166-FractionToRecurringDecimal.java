021503515e9977cc41c9a4c371207750051ff7cb
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-02 16:03:32 +0530

    Create 166-FractionToRecurringDecimal

---
+++ 166-FractionToRecurringDecimal/166-FractionToRecurringDecimal.java
@@ -0,0 +1,40 @@
+// Last updated: 8/2/2025, 4:02:53 PM
+class Solution {
+    public String fractionToDecimal(int numerator, int denominator) {
+        StringBuilder ans = new StringBuilder();
+        if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0))
+            ans.append("-");
+        
+        long num = (long) Math.abs((long) numerator);
+        long den = (long) Math.abs((long) denominator);
+        
+        long quotient = num / den;
+        long remainder = num % den;
+        
+        ans.append(String.valueOf(quotient));
+        
+        if (remainder == 0)
+            return ans.toString();
+        
+        ans.append(".");
+        
+        Map<Long, Integer> map = new HashMap<>();
+        
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                int pos = map.get(remainder);
+                ans.insert(pos, "(");
+                ans.append(")");
+                return ans.toString();
+            } else {
+                map.put(remainder, ans.length());
+                remainder *= 10;
+                quotient = remainder / den;
+                remainder %= den;
+                ans.append(String.valueOf(quotient));
+            }
+        }
+        
+        return ans.toString();
+    }
+}
