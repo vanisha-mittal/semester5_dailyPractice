26b81b2ee040f65963f77833eefd6be39afbbb68
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-08 23:03:54 +0530

    Create 450-DeleteNodeInABst

---
+++ 450-DeleteNodeInABst/450-DeleteNodeInABst.java
@@ -0,0 +1,50 @@
+// Last updated: 8/8/2025, 11:03:13 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(root.val<key){
+            root.right=deleteNode(root.right,key);
+        }
+        else if(root.val>key){
+            root.left=deleteNode(root.left,key);
+        }
+        else{
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            else{
+                int max=max(root.left);
+                root.left=deleteNode(root.left,max);
+                root.val=max;
+            }
+        }
+        return root;
+    }
+    public int max(TreeNode root){
+        if(root==null){
+            return Integer.MIN_VALUE;
+        }
+        int max=max(root.right);
+        return Math.max(max,root.val);
+    }
+}
