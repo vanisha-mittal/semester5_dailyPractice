a2f57b9c9c65aa5c4c35cce02f1ec6ee0b933c32
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-13 11:33:09 +0530

    Create 968-BinaryTreeCameras

---
+++ 968-BinaryTreeCameras/968-BinaryTreeCameras.java
@@ -0,0 +1,42 @@
+// Last updated: 8/13/2025, 11:32:26 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int camera=0;
+    public int minCameraCover(TreeNode root) {
+        // has camera=1, need camera=-1, covered =0
+        int c=minCamera(root);
+        if(c==-1){
+            camera++;
+        }
+        return camera;
+    }
+    public int minCamera(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=minCamera(root.left);
+        int right=minCamera(root.right);
+        if(left==-1|| right==-1){
+            camera++;
+            return 1;
+        }
+        if(left==1||right==1){
+            return 0;
+        }
+        return -1;
+    }
+}
