65970ec4f20a6259755b8a45af70f4d2029dfdb5
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-14 12:00:26 +0530

    Create 449-SerializeandDeserializeBST

---
+++ 449-SerializeandDeserializeBST/449-SerializeandDeserializeBST.java
@@ -0,0 +1,55 @@
+// Last updated: 8/14/2025, 11:59:44 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb=new StringBuilder();
+        build(root,sb);
+        return sb.toString();
+    }
+    private void build(TreeNode root,StringBuilder sb){
+        if(root==null){
+            sb.append("null,");
+            return;
+        }
+        sb.append(root.val).append(",");
+        build(root.left,sb);
+        build(root.right,sb);
+    }
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if(data==null||data.isEmpty()){
+            return null;
+        }
+        String[] arr=data.split(",");
+        return parse(arr);
+    }
+    int idx=0;
+    public TreeNode parse(String[] arr){
+        if(idx>arr.length){
+            return null;
+        }
+        String s=arr[idx++];
+        if(s.equals("null")){
+            return null;
+        }
+        TreeNode node=new TreeNode(Integer.parseInt(s));
+        node.left=parse(arr);
+        node.right=parse(arr);
+        return node;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser = new Codec();
+// Codec deser = new Codec();
+// TreeNode ans = deser.deserialize(ser.serialize(root));
