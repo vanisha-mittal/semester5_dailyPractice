0b40d0c75b9fd45995b11588cc391a6c0c8690a4
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-08-20 12:39:37 +0530

    Create 23-MergekSortedLists

---
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -0,0 +1,33 @@
+// Last updated: 8/20/2025, 12:38:52 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b) -> a.val-b.val);
+        ListNode Dummy=new ListNode();
+        ListNode temp=Dummy;
+        for(int i=0;i<lists.length;i++){
+            if(lists[i]!=null){
+                pq.add(lists[i]);
+            }
+        }
+        while(!pq.isEmpty()){
+            ListNode val=pq.poll();
+            Dummy.next=val;
+            Dummy=Dummy.next;
+            if(val.next!=null){
+                pq.add(val.next);
+            }
+        }
+        return temp.next;
+
+    }
+}
