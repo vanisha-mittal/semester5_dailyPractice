59326a522fdf60556ba2b127a48962c917cc84aa
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-02 23:05:40 +0530

    Create 1427-AllElementsInTwoBinarySearchTrees

---
+++ 1427-AllElementsInTwoBinarySearchTrees/1427-AllElementsInTwoBinarySearchTrees.java
@@ -0,0 +1,51 @@
+// Last updated: 9/2/2025, 11:04:49 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+
+        inOrder(root1, l1);
+        inOrder(root2, l2);
+
+        int i=0, j=0;
+        while(i<l1.size() && j<l2.size()){
+            if(l1.get(i) < l2.get(j)){
+                result.add(l1.get(i++));
+            } else {
+                result.add(l2.get(j++));
+            }
+        } 
+        while(i<l1.size()) {
+            result.add(l1.get(i++));
+        }
+        while(j<l2.size()) {
+            result.add(l2.get(j++));
+        }
+
+        return result;
+    }
+
+    public void inOrder(TreeNode root, List<Integer> list){
+        if(root == null) return;
+
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+}
