6350a0ad494429be7ecb67346c039ba89feccc62
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-04 11:10:23 +0530

    File updated at 9/4/2025, 11:09:32 AM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,24 +1,24 @@
-// Last updated: 7/31/2025, 5:21:24 PM
+// Last updated: 9/4/2025, 11:09:32 AM
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount+1];
-        dp[0] = 0;
-        for(int i=1; i<dp.length;i++){
-            int min=amount+1;
-            for(int coin:coins){
-                if(i-coin<0){
-                    continue;
+    public int longestCommonSubsequence(String text1, String text2) {
+        return LCS(text1,text2);
+    }
+    public static int LCS(String s1,String s2){
+        int[][] dp=new int[s1.length()+1][s2.length()+1];
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                int ans=0;
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    ans=1+dp[i-1][j-1];
                 }
-                int curr = 1 + dp[i-coin];
-                if(curr < min){
-                    min = curr;
+                else{
+                    int f=dp[i-1][j];
+                    int s=dp[i][j-1];
+                    ans=Math.max(f,s);
                 }
+                dp[i][j]=ans;
             }
-            dp[i] = min;
         }
-        if(dp[amount]>amount){
-            return -1;
-        }
-        return dp[amount];
+        return dp[dp.length-1][dp[0].length-1];
     }
 }
