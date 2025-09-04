e7e1de436563b93d5a6895c2891d564ee6d8f698
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-04 11:12:39 +0530

    File updated at 9/4/2025, 11:11:43 AM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,14 +1,14 @@
-// Last updated: 9/4/2025, 11:09:32 AM
+// Last updated: 9/4/2025, 11:11:43 AM
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        return LCS(text1,text2);
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        return LCS(nums1,nums2);
     }
-    public static int LCS(String s1,String s2){
-        int[][] dp=new int[s1.length()+1][s2.length()+1];
+    public static int LCS(int[] s1,int[] s2){
+        int[][] dp=new int[s1.length+1][s2.length+1];
         for(int i=1;i<dp.length;i++){
             for(int j=1;j<dp[0].length;j++){
                 int ans=0;
-                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                if(s1[i-1]==s2[j-1]){
                     ans=1+dp[i-1][j-1];
                 }
                 else{
