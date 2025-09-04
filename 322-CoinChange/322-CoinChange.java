f586b56135806c7ec11c21e48579500ac11a3b19
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-04 12:11:36 +0530

    File updated at 9/4/2025, 12:10:45 PM

--- 322-CoinChange/322-CoinChange.java
+++ 322-CoinChange/322-CoinChange.java
@@ -1,28 +1,25 @@
-// Last updated: 9/4/2025, 11:35:23 AM
+// Last updated: 9/4/2025, 12:10:45 PM
 class Solution {
     public int change(int amount, int[] coins) {
-        int[][] dp=new int[amount+1][coins.length];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
-        }
-        return Coin_Change(coins,amount,0,dp);
+        return Coin_Change(coins, amount);
     }
-    public static int Coin_Change(int[] coin, int amount, int i, int [][] dp){
-        if(amount==0){
-            return 1;
+
+    public static int Coin_Change(int[] coin, int amount) {
+        int[][] dp = new int[coin.length+1][amount + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 1;
         }
-        if(i==coin.length){
-            return 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                int inc = 0;
+                int exc = 0;
+                if (j >= coin[i-1]) {
+                    inc = dp[i][j - coin[i - 1]];
+                }
+                exc = dp[i - 1][j];
+                dp[i][j] = inc + exc;
+            }
         }
-        if(dp[amount][i]!=-1){
-            return dp[amount][i];
-        }
-        int inc=0;
-        int exc=0;
-        if(amount>=coin[i]){
-            inc=Coin_Change(coin, amount-coin[i], i, dp);
-        }
-        exc=Coin_Change(coin, amount, i+1, dp);
-        return dp[amount][i]=exc+inc;
+        return dp[dp.length - 1][dp[0].length - 1];
     }
 }
