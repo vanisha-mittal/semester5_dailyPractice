fe73c8f0e672de9dbd0ab8a410d565f91dc8f5a2
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-10 12:19:59 +0530

    File updated at 9/10/2025, 12:19:05 PM

--- 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
+++ 222-CountCompleteTreeNodes/222-CountCompleteTreeNodes.java
@@ -1,24 +1,30 @@
-// Last updated: 9/10/2025, 11:59:53 AM
+// Last updated: 9/10/2025, 12:19:05 PM
 class Solution {
-    public int minPathSum(int[][] grid) {
+    public int minFallingPathSum(int[][] grid) {
         int[][] dp=new int[grid.length][grid[0].length];
         for(int[] i:dp){
-            Arrays.fill(i,-1);
+            Arrays.fill(i,-1000000);
         }
-        return Minimum_Path(grid,0,0,dp);
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<grid[0].length;i++){
+            int a=Minimum_Path(grid,0,i,dp);
+            ans=Math.min(a,ans);
+        }
+        return ans;
     }
     public static int Minimum_Path(int [][] grid, int cr, int cc,int[][]dp){
-        if(cr==grid.length-1 && cc==grid[0].length-1){
-            return grid[cr][cc];
-        }
-        if(cc==grid[0].length || cr==grid.length){
+        if(cc==grid[0].length || cc<0){
             return Integer.MAX_VALUE;
         }
-        if(dp[cr][cc]!=-1){
+        if(cr==grid.length-1){
+            return grid[cr][cc];
+        }
+        if(dp[cr][cc]!=-1000000){
             return dp[cr][cc];
         }
         int d=Minimum_Path(grid,cr+1,cc,dp);
-        int r=Minimum_Path(grid,cr,cc+1,dp);
-        return dp[cr][cc]=Math.min(d,r)+grid[cr][cc];
+        int r=Minimum_Path(grid,cr+1,cc+1,dp);
+        int l=Minimum_Path(grid,cr+1,cc-1,dp);
+        return dp[cr][cc]=Math.min(d,Math.min(r,l))+grid[cr][cc];
     }
 }
