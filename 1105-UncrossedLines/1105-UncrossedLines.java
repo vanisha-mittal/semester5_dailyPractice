4cab051f83d62db7688480334c044308b97358a8
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-10 12:40:01 +0530

    Create 1105-UncrossedLines

---
+++ 1105-UncrossedLines/1105-UncrossedLines.java
@@ -0,0 +1,24 @@
+// Last updated: 9/10/2025, 12:39:07 PM
+class Solution {
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        return LCS(nums1,nums2);
+    }
+    public static int LCS(int[] s1,int[] s2){
+        int[][] dp=new int[s1.length+1][s2.length+1];
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                int ans=0;
+                if(s1[i-1]==s2[j-1]){
+                    ans=1+dp[i-1][j-1];
+                }
+                else{
+                    int f=dp[i-1][j];
+                    int s=dp[i][j-1];
+                    ans=Math.max(f,s);
+                }
+                dp[i][j]=ans;
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
