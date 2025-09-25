cfea42252548fe5df8321cb2b307e6fa75529e03
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-25 21:53:43 +0530

    Create 434-NumberofSegmentsinaString

---
+++ 434-NumberofSegmentsinaString/version/java/434-NumberofSegmentsinaString_20250925_215242.java
@@ -0,0 +1,12 @@
+// Last updated: 9/25/2025, 9:52:42 PM
+class Solution {
+    public int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
