106741f23bbe26a7160ad51aacfddd715b983514
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-25 22:06:53 +0530

    Create 459-RepeatedSubstringPattern

---
+++ 459-RepeatedSubstringPattern/version/java/459-RepeatedSubstringPattern_20250925_220552.java
@@ -0,0 +1,8 @@
+// Last updated: 9/25/2025, 10:05:52 PM
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String sub = doubled.substring(1, doubled.length() - 1);
+        return sub.contains(s);
+    }
+}
