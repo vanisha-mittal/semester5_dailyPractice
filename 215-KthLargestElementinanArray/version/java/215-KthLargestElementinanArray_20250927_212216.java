b921e9755e05cea9c943b269b7f5e775622cd02b
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-09-27 21:23:18 +0530

    Create 215-KthLargestElementinanArray

---
+++ 215-KthLargestElementinanArray/version/java/215-KthLargestElementinanArray_20250927_212216.java
@@ -0,0 +1,19 @@
+// Last updated: 9/27/2025, 9:22:16 PM
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int[] count = new int[20001];
+
+        for (int num : nums) 
+            count[num + 10000]++;
+
+        for (int i = count.length - 1; i >= 0; i--) 
+            if (count[i] > 0) {
+                k -= count[i];
+                if (k <= 0){
+                    return i - 10000;
+                }      
+            }
+        
+        return -1;
+    }
+}
