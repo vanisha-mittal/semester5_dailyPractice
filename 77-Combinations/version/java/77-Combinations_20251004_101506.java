f76d663bbda77266d0f0ce8cc1fce99571ab34b5
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-10-04 10:16:12 +0530

    Create 77-Combinations

---
+++ 77-Combinations/version/java/77-Combinations_20251004_101506.java
@@ -0,0 +1,25 @@
+// Last updated: 10/4/2025, 10:15:06 AM
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> arr=new ArrayList<>();
+		List<Integer> curr=new ArrayList<>();
+        Arrays.sort(candidates);
+		Permutation(candidates,target,curr,arr,0);
+        return arr;
+}
+	public static  void Permutation(int[] coin,int amount,List<Integer> curr, List<List<Integer>> arr,int indx) {
+		if(amount==0) {
+			arr.add(new ArrayList<>(curr)); 
+			return;
+		}
+		for(int i=indx;i<coin.length;i++) {
+			if(amount>=coin[i]) {
+                if(i>indx && coin[i]==coin[i-1]) continue;
+                curr.add(coin[i]);
+				Permutation(coin,amount-coin[i],curr,arr,i+1);
+                curr.remove(curr.size() - 1);
+			}
+		}
+	}
+
+}
