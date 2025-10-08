52cb60e04a855af388e179c510b810bfb28ddf14
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-10-08 10:39:14 +0530

    Create 146-LRUCache

---
+++ 146-LRUCache/version/java/146-LRUCache_20251008_103806.java
@@ -0,0 +1,23 @@
+// Last updated: 10/8/2025, 10:38:06 AM
+class LRUCache {
+    private final int capacity;
+    private final LinkedHashMap<Integer, Integer> map;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > LRUCache.this.capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
