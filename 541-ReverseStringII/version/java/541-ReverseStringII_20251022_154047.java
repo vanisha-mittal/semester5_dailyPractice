3075f1162be16945f6748cc1bdc9ac38d2242165
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-10-22 15:42:00 +0530

    Create 541-ReverseStringII

---
+++ 541-ReverseStringII/version/java/541-ReverseStringII_20251022_154047.java
@@ -0,0 +1,29 @@
+// Last updated: 10/22/2025, 3:40:47 PM
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[] str = s.toCharArray();
+        int n = s.length();
+
+        for (int i = 0; i < n; i += 2 * k) {
+            if (i + k - 1 <= n - 1) {
+                reverseK(i, i + k - 1, str);
+            } else {
+                reverseK(i, n - 1, str);
+
+            }
+        }
+
+        String revStr = new String(str);
+        return revStr;
+    }
+
+    public void reverseK(int i, int j, char[] str) {
+        while (i < j) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
