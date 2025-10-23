d928b57acd0609f488d0ddbad49836d214cd1d73
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-10-23 22:47:25 +0530

    Create 3461-CheckIfDigitsAreEqualinStringAfterOperationsI

---
+++ 3461-CheckIfDigitsAreEqualinStringAfterOperationsI/version/java/3461-CheckIfDigitsAreEqualinStringAfterOperationsI_20251023_224613.java
@@ -0,0 +1,18 @@
+// Last updated: 10/23/2025, 10:46:13 PM
+class Solution {
+  public boolean hasSameDigits(String s) {
+    int[] arr = new int[s.length()];
+
+    for (int i = 0; i < arr.length; i++)
+      arr[i] = s.charAt(i) - '0';
+    
+    for (int length = arr.length; length > 2; length--) {
+      for (int i = 0; i < length - 1; i++) {
+        arr[i] = (arr[i] + arr[i + 1]) % 10;
+      }
+    }
+
+    return arr[0] == arr[1];
+      
+  }
+}
