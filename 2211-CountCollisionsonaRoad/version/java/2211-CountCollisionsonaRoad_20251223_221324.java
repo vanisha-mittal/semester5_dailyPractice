c2009147f9b18eb91a883eb75a5af5cd6867c730
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-12-23 22:14:53 +0530

    Create 2211-CountCollisionsonaRoad

---
+++ 2211-CountCollisionsonaRoad/version/java/2211-CountCollisionsonaRoad_20251223_221324.java
@@ -0,0 +1,16 @@
+// Last updated: 12/23/2025, 10:13:24 PM
+1class Solution {
+2    public int countCollisions(String D) {
+3        final int n=D.length();
+4        if (n==1) return 0;
+5        int l=0, r=n-1;
+6        while (l<r && D.charAt(l)=='L') l++;
+7        while (l<r && D.charAt(r)=='R') r--;
+8        if (l>=r) return 0;
+9        int col=0;
+10        for( ; l<=r; l++){
+11           col+=(D.charAt(l)!='S'?1:0);
+12        }
+13        return col;  
+14    }
+15}
