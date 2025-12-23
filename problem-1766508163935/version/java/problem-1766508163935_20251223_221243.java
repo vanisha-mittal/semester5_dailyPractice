3b795fb2e09a1d6a7630a5137893e4dd08fa3494
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2025-12-23 22:14:02 +0530

    Create

---
+++ problem-1766508163935/version/java/problem-1766508163935_20251223_221243.java
@@ -0,0 +1,22 @@
+// Last updated: 12/23/2025, 10:12:43 PM
+1public class Solution {
+2    public int findMinimumOperations(String s1, String s2, String s3) {
+3        int len = Math.min(s1.length(), Math.min(s2.length(), s3.length())); 
+4        int sum = s1.length() + s2.length() + s3.length(); 
+5        if (s1.charAt(0) != s2.charAt(0) || s1.charAt(0) != s3.charAt(0) || s2.charAt(0) != s3.charAt(0)) {
+6            return -1;
+7        }
+8
+9        for (int i = 0; i < len; i++) {
+10            if (s1.charAt(i) == s2.charAt(i) && s2.charAt(i) == s3.charAt(i)) {
+11                sum -= 3; 
+12            } else {
+13                break; 
+14            }
+15        }
+16
+17        return sum; 
+18    }
+19}
+20
+21
