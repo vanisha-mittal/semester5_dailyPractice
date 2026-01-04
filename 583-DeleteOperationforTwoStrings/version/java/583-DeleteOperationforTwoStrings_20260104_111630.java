ac2c9a4a273930c0d533030f0eb3c804d4aa741e
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2026-01-04 11:17:50 +0530

    Create 583-DeleteOperationforTwoStrings

---
+++ 583-DeleteOperationforTwoStrings/version/java/583-DeleteOperationforTwoStrings_20260104_111630.java
@@ -0,0 +1,21 @@
+// Last updated: 1/4/2026, 11:16:30 AM
+1class Solution {
+2    public int minDistance(String word1, String word2) {
+3        int n = word1.length();
+4        int m = word2.length();
+5        int[][] dp = new int[n + 1][m + 1];
+6        
+7        for (int i = 1; i <= n; i++) {
+8            for (int j = 1; j <= m; j++) {
+9                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+10                    dp[i][j] = 1 + dp[i - 1][j - 1];
+11                } else {
+12                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+13                }
+14            }
+15        }
+16        
+17        int lcs = dp[n][m];
+18        return n + m - 2 * lcs;
+19    }
+20}
