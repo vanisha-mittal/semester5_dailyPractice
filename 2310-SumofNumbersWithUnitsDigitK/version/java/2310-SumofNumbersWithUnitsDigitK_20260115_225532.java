6c7ad15156a35782df1fae31905d34236848b7c5
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2026-01-15 22:56:55 +0530

    Create 2310-SumofNumbersWithUnitsDigitK

---
+++ 2310-SumofNumbersWithUnitsDigitK/version/java/2310-SumofNumbersWithUnitsDigitK_20260115_225532.java
@@ -0,0 +1,15 @@
+// Last updated: 1/15/2026, 10:55:32 PM
+1class Solution {
+2    public int minimumNumbers(int num, int k) {
+3        if(num==0){
+4            return 0;
+5        }
+6        for(int i=1;i<=10;i++){
+7            if((num-(i*k))%10==0 && i * k <= num){
+8                return i;
+9            }
+10        }
+11
+12        return -1;
+13    }
+14}
