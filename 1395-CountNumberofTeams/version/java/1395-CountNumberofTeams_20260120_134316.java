b2fa11aae7984ec4f0153bb932165616ce4b0ad3
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2026-01-20 13:44:37 +0530

    Create 1395-CountNumberofTeams

---
+++ 1395-CountNumberofTeams/version/java/1395-CountNumberofTeams_20260120_134316.java
@@ -0,0 +1,14 @@
+// Last updated: 1/20/2026, 1:43:16 PM
+1class Solution {
+2    public int numTeams(int[] rating) {
+3        int c=0;
+4        for(int i=0;i<rating.length;i++){
+5            for(int j=i+1;j<rating.length;j++){
+6                for(int k=j+1;k<rating.length;k++){
+7                    if((rating[i]<rating[j] && rating[j]<rating[k])||(rating[i]>rating[j] && rating[j]>rating[k])) c++;
+8                }
+9            }
+10        }
+11        return c;
+12    }
+13}
