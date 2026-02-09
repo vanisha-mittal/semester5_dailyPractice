ca44407c44d9b20972434c662afa194a21a7c5f9
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2026-02-09 15:24:27 +0530

    Create 2509-CycleLengthQueriesinaTree

---
+++ 2509-CycleLengthQueriesinaTree/version/java/2509-CycleLengthQueriesinaTree_20260209_152304.java
@@ -0,0 +1,24 @@
+// Last updated: 2/9/2026, 3:23:04 PM
+1class Solution {
+2    public int[] cycleLengthQueries(int n, int[][] queries) {
+3        int[] ans = new int[queries.length];
+4        int x = 0;
+5        for (int i = 0; i < queries.length; i++) {
+6            int a = queries[i][0];
+7            int b = queries[i][1];
+8            int ah = 0;
+9            int bh = 0;
+10            while (a != b) {
+11                if (a > b) {
+12                    a>>=1;
+13                    ah++;
+14                } else {
+15                    b>>=1;
+16                    bh++;
+17                }
+18            }
+19            ans[x++] = bh + ah + 1;
+20        }
+21        return ans;
+22    }
+23}
