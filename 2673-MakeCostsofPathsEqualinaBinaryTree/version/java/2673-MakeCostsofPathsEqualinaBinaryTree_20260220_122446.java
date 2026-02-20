e47fc0196da4bd9167a9ed90a2dee25a53b5738c
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2026-02-20 12:26:10 +0530

    Create 2673-MakeCostsofPathsEqualinaBinaryTree

---
+++ 2673-MakeCostsofPathsEqualinaBinaryTree/version/java/2673-MakeCostsofPathsEqualinaBinaryTree_20260220_122446.java
@@ -0,0 +1,12 @@
+// Last updated: 2/20/2026, 12:24:46 PM
+1class Solution {
+2    public int minIncrements(int n, int[] A) {
+3        int res = 0;
+4        for (int i = n / 2 - 1; i >= 0; --i) {
+5            int l = i * 2 + 1, r = i * 2 + 2;
+6            res += Math.abs(A[l] - A[r]);
+7            A[i] += Math.max(A[l], A[r]);
+8        }
+9        return res;
+10    }
+11}
