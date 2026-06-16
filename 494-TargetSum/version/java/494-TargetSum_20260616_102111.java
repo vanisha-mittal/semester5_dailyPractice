30ca052d3f86d7c0352df7fb518675734fec6c00
Vanisha Mittal <166028321+vanisha-mittal@users.noreply.github.com>
2026-06-16 10:21:11 +0530

    Create 494-TargetSum

---
+++ 494-TargetSum/version/java/494-TargetSum_20260616_102111.java
@@ -0,0 +1,12 @@
+// Last updated: 6/16/2026, 10:21:11 AM
+1class Solution {
+2    public int findTargetSumWays(int[] nums, int target) {
+3        return sum(nums,0,target,0);
+4    }
+5    public static int sum(int[] nums, int i, int target, int curr){
+6        if(i>=nums.length && curr==target) return 1;
+7        if(i>=nums.length) return 0;
+8
+9        return sum(nums,i+1,target,curr+nums[i])+sum(nums,i+1,target,curr-nums[i]);
+10    }
+11}
